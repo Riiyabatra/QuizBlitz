5116c853c21e97524581de87c0bc3f06fd2aaa54
Yash Agrawal <yagrawal@Yash.local>
2024-03-02 14:52:41 -0500

    Setting up SpringBoot project

---
+++ backend/src/main/java/com/example/quizblitz/QuizblitzApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizblitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizblitzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizblitzApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/quizblitz/QuizblitzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizblitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizblitzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
